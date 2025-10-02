f734261bc2217d96ab7091edfe9424120925a16a
Pawel K <pawelkonarzewski13@gmail.com>
2025-10-02 21:15:31 +0700

    Add Min number field validation

---
+++ TabataSimpleCopyV1Cloude/Shared/Models/Min.swift
@@ -0,0 +1,28 @@
+//
+//  MinValue.swift
+//  TabataSimpleCopyV1Cloude
+//
+//  Created by Pawel Konarzewski on 02/10/2025.
+//
+
+@propertyWrapper
+struct Min<T: Comparable> {
+    var min: T
+    var value: T
+
+    init(wrappedValue: T,_ min: T) {
+        value = wrappedValue
+        self.min = min
+    }
+
+    var wrappedValue: T {
+        get { value }
+        set {
+            if newValue < min {
+                value = min
+            } else {
+                value = newValue
+            }
+        }
+    }
+}
